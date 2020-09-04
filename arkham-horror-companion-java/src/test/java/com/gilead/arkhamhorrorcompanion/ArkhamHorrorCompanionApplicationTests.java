31bd4e2bfafb8f7a5c5632b9ef9647b570ba2ac2
John Wyatt <JohnWyatt@gmail.com>
2020-09-04 14:35:15 -0400

    added initial spring boot java application project

---
+++ arkham-horror-companion-java/src/test/java/com/gilead/arkhamhorrorcompanion/ArkhamHorrorCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilead.arkhamhorrorcompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkhamHorrorCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
