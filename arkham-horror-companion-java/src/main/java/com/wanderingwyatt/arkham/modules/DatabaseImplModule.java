13e6eeec90544f601cc6a51488fd1fa092a88538
John Wyatt <JohnWyatt@gmail.com>
2020-09-28 22:04:57 -0400

    Added configurations to enable ehcache second level caching in hibernate.

--- arkham-horror-companion-java/src/main/java/com/wanderingwyatt/arkham/modules/DatabaseImplModule.java
+++ arkham-horror-companion-java/src/main/java/com/wanderingwyatt/arkham/modules/DatabaseImplModule.java
@@ -3,6 +3,8 @@ package com.wanderingwyatt.arkham.modules;
 import java.util.Set;
 import javax.inject.Named;
 import org.hibernate.SessionFactory;
+import org.hibernate.cache.jcache.ConfigSettings;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import dagger.Module;
 import dagger.Provides;
@@ -16,17 +18,24 @@ public class DatabaseImplModule {
 	
 	@Provides
 	@Named("configuration")
-	Configuration provideConfiguration(@Named("annotatedPersistenceClasses") Set<Class<?>> annotatedPersistenceClasses) {
+	Configuration provideConfiguration(@Named("annotatedPersistenceClasses") Set<Class<?>> annotatedPersistenceClasses, 
+			CacheConfigurer cacheConfigurer) {
+		
+		// Configure all the caches before configuring hibernate for caching.
+		cacheConfigurer.configure();
+		
 		Configuration configuration = new Configuration();
-		configuration.setProperty("hibernate.connection.datasource", "java:comp/env/jdbc/arkhamHorrorDb");
-		configuration.setProperty("dialect", "org.hibernate.dialect.H2");
-		configuration.setProperty("hibernate.hbm2ddl.auto", "update");
-		configuration.setProperty("hibernate.current_session_context_class", "thread");
-		configuration.setProperty("show_sql", "true");
-		configuration.setProperty("hibernate.connection.pool_size", "10");
+		configuration.setProperty(AvailableSettings.DATASOURCE, "java:comp/env/jdbc/arkhamHorrorDb");
+		configuration.setProperty(AvailableSettings.DIALECT, "org.hibernate.dialect.H2Dialect");
+		configuration.setProperty(AvailableSettings.HBM2DDL_AUTO, "update");
+		configuration.setProperty(AvailableSettings.SHOW_SQL, "true");
+		configuration.setProperty(AvailableSettings.USE_SECOND_LEVEL_CACHE, "true");
+		configuration.setProperty(AvailableSettings.USE_QUERY_CACHE, "true");
+		configuration.setProperty(AvailableSettings.CACHE_REGION_FACTORY, "org.hibernate.cache.jcache.JCacheRegionFactory");
+		configuration.setProperty(ConfigSettings.PROVIDER, "org.ehcache.jsr107.EhcacheCachingProvider");
 	    
 		// Add all the annotatedPersistenceClasses
 		annotatedPersistenceClasses.forEach(configuration::addAnnotatedClass);
 		return configuration;
-	}
+	}	
 }
