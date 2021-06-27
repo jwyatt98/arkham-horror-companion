f570a35dc27f0dbd023ebd5be8382e2c88e06081
John Wyatt <JohnWyatt@gmail.com>
2021-06-27 05:58:07 -0400

    made custom conversion exception for when database field conversion fails. Moved converter to dao package.

---
+++ arkham-horror-companion-java/src/main/java/com/wanderingwyatt/arkham/dao/ConversionException.java
@@ -0,0 +1,29 @@
+package com.wanderingwyatt.arkham.dao;
+
+public class ConversionException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1093210365642995140L;
+
+	public ConversionException() {
+	}
+
+	public ConversionException(String message) {
+		super(message);
+	}
+
+	public ConversionException(Throwable cause) {
+		super(cause);
+	}
+
+	public ConversionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ConversionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
