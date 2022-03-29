8fe67ef013662e90d69f3bdfbbbe98238684079a
John Wyatt <JohnWyatt@gmail.com>
2022-03-29 18:40:36 -0400

    use correct property name

--- arkham-horror-companion-java/src/test/java/com/wanderingwyatt/arkham/dao/TestDaoModule.java
+++ arkham-horror-companion-java/src/test/java/com/wanderingwyatt/arkham/dao/TestDaoModule.java
@@ -12,14 +12,14 @@ public class TestDaoModule extends DaoModule {
 	@Override
 	protected Configuration getConfiguration() {
 		Configuration configuration = new Configuration();
-		configuration.setProperty("connection.driver_class","org.h2.Driver");
+		configuration.setProperty("hibernate.connection.driver_class","org.h2.Driver");
 		configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:db1");                                
 		configuration.setProperty("hibernate.connection.username", "sa");     
 		configuration.setProperty("hibernate.connection.password", "");
 		configuration.setProperty("dialect", "org.hibernate.dialect.H2Dialect");
 		configuration.setProperty("hibernate.hbm2ddl.auto", "create");
 		configuration.setProperty("hibernate.current_session_context_class", "thread");
-		configuration.setProperty("show_sql", "true");
+		configuration.setProperty("hibernate.show_sql", "true");
 		configuration.setProperty("hibernate.connection.pool_size", "10");
 		configuration.setProperty("hibernate.cache.use_second_level_cache", "false");
 		return configuration;
